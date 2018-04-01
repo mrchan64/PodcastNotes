95c83c0e6c3958be3567f17edf7830c44ca8f8d5
Ravi Sheth <r1sheth@ucds.edu>
2018-03-31 18:23:15 -0700

    note and sub print

--- public/socket.js
+++ public/socket.js
@@ -1,6 +1,14 @@
 var ws = new WebSocket('ws://localhost:9000/websocket');
 
-var notes = $('#notes');
+var notesArr = [];
+//var notes = $('#notes');
+
+var subtitleTags = [];
+
+var dictionary = {};
+var chrononotes = {};
+var index = 0;
+
 
 var subtitleData = {
   text:[],
@@ -10,17 +18,80 @@ var subtitleData = {
 ws.onmessage = function(event){
   var data = JSON.parse(event.data);
   if(data.tag == 'subtitles'){
-    subtitleData.text.push(data.text);
-    subtitleData.timestamps.push(data.time);
+    subtitleData.text = data.text;
+    subtitleData.timestamps = data.time;
   }else if(data.tag == 'notes'){
-    var bullet = $('p');
-    bullet.classed('note-'+data.indent, true)
-    bullet.html(data.text);
-    notes.append(bullet);
+    dictionary = data.notes;
+    Object.keys(dictionary).forEach((key)=>{
+      chrononotes[dictionary[key].wavnum] = key;
+    });
   }
 }
 
-$('#inplayer_html5_api').on('timeupdate', ()=>{
-  time = this.currentTime;
-  console.log(time)
-})
+setTimeout(function(){
+    var video = document.getElementById('inplayer_html5_api');
+
+    video.ontimeupdate = function(){timeOnVideo(video)};
+
+}, 1000);
+
+
+
+function timeOnVideo(video){
+  var time = video.currentTime;
+  console.log(time);
+  if(dictionary[time] != undefined){
+    newIndex = dictionary[time];
+    if(currIndex != newIndex){
+      $(notesArr[newIndex]).addClass("highlight");
+      $(notesArr[currIndex]).removeClass("highlight");
+    }
+    currIndex = newIndex;
+  }
+
+  for( var i = 0; i<subtitleData.text.length; i++){
+    if(time<subtitleData.timestamps[i]){
+      //subtitleTags[i];
+      console.log(i);
+      $('#belowbar').animate({scrollTop: subtitleTags[i].prop("offsetTop")}, 500);
+      var order = chrononotes[i];
+      if(order){
+        var target = dictionary[order].elem;
+        if(!elem.hasClass('hightlight')){
+          $('.note').removeClass('highlight');
+          elem.addClass('highlight');
+        }
+      }
+
+      break;
+    }
+  }
+
+  //$().text(currentTime)
+  //$().text(duration)
+
+}
+
+function printSubtitle(){
+  subtitleData.text.forEach((text)=>{
+    var curTag = $("<p>"+ text+"</p>");
+    subtitleTags.push(curTag);
+    $('#belowbar').append(curTag);
+  })
+}
+
+function printNotes(){
+  // by topic
+  var notes = $('#notes');
+  //$('.note').remove();
+  Object.keys(dictionary).sort((a,b)=>{
+    return parseInt(a.replace('order',''))>parseInt(b.replace('order',''));
+  }).forEach((key)=>{
+    var note = $('<p></p>');
+    note.html(dictionary[key].text);
+    note.addClass('note-'+dictionary[key].importance);
+    note.addClass('note');
+    dictionary[key].elem = note;
+    notes.append(note);
+  })
+}
