3e8a7337f88ecec2ddd4b78f6e770acc3647df9a
Matt Chan <wildcatrobotics2011@gmail.com>
2018-04-01 21:52:43 -0700

    final pres

--- public/submit.js
+++ public/submit.js
@@ -15,12 +15,12 @@ var demo1 = function(){
 
 var demo2 = function(){
   $('#center-stage').animate({opacity:0}, 400, 'easeInCubic', ()=>{
-    window.location.replace(window.location+'loader/?url=http%3A%2F%2Fpodcast-media.ucsd.edu%2FPodcasts%2Fwi18%2Fcse105_a00%2Fcse105_a00_wi18-03162018-0800.mp4')
+    window.location.replace(window.location+'loader/?url=http%3A%2F%2Fpodcast-media.ucsd.edu%2FPodcasts%2Fwi18%2Fbieb130_a00%2Fbieb130_a00_wi18-02152018-1100.mp4')
   })
 }
 
-var demo2 = function(){
+var demo3 = function(){
   $('#center-stage').animate({opacity:0}, 400, 'easeInCubic', ()=>{
-    window.location.replace(window.location+'loader/?url=http%3A%2F%2Fpodcast-media.ucsd.edu%2FPodcasts%2Fwi18%2Fcse105_a00%2Fcse105_a00_wi18-03162018-0800.mp4')
+    window.location.replace(window.location+'loader/?url=http%3A%2F%2Fpodcast-media.ucsd.edu%2FPodcasts%2Fwi18%2Fusp143_a00%2Fusp143_a00_wi18-03132018-0930.mp4')
   })
 }
